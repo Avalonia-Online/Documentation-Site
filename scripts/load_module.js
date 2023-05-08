fc9c0e8c32949c27065f0a19198fd89d07db5eb1
Knight <113319174+Original-Developer@users.noreply.github.com>
2023-05-08 18:48:23 +0200

    Load modules test

--- scripts/load_module.js
+++ scripts/load_module.js
@@ -1,5 +1,5 @@
-!function loadModule (module) {
+function loadModule (module) {
     window.fetch(`https://avalonia-online.github.io/Documentation-Site/modules/${module}.html`)
         .then(response => response.text())
         .then(text => document.getElementById("content").innerHTML = text);
-}("index");
+}
