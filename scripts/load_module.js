a258923c25506746ba48d6c753f6da3e8bff1ab8
Knight <113319174+Original-Developer@users.noreply.github.com>
2023-05-08 18:14:41 +0200

    Fix load module (attempt)

--- scripts/load_module.js
+++ scripts/load_module.js
@@ -1,5 +1,5 @@
 !function loadModule (module) {
-    window.fetch(`../modules/${module}.html`)
+    window.fetch(`../Documentation-Site/modules/${module}.html`)
         .then(response => response.text())
         .then(text => document.getElementById("content").innerHTML = text);
 }("index");
